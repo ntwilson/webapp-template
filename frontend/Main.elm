d97ddd949a33fb401be70cb227b6d5edf5e948fb
nathan wilson <nathan.wilson@marquette.edu>
2016-06-23 13:44:15 -0500

    get the elm app working the way i want it.

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -10,75 +10,99 @@ main =
 
 -- MODEL
 
-type ViewMode = StillEntering | GoodPassword | BadPassword
-
 type alias Model =
   { viewMode: ViewMode,
     name : String,
     password : String,
-    passwordAgain : String
-  }
+    passwordAgain : String }
 
+type ViewMode = ZeroPassword | GoodPassword | BadPassword | Done | YDumb
 
-model : Model
-model =
-  Model StillEntering "" "" ""
+viewMode pwd pwdAgain =
+  case (pwd, pwdAgain) of
+    ("", _) -> ZeroPassword
+    (_, "") -> ZeroPassword
+    (p1, p2) -> if (p1 == p2) then GoodPassword else BadPassword
 
+passwordsMatch model = 
+  model.password == model.passwordAgain
+
+newModel = 
+  let (pwd, pwdAgain) = ("", "") in
+  Model (viewMode pwd pwdAgain) "" pwd pwdAgain
+
+model = newModel
 
 -- UPDATE
 
-type Msg
-    = Submit
-    | Name String
-    | Password String
-    | PasswordAgain String
+type Msg = 
+  Submit
+  | Reset
+  | Name String
+  | Password String
+  | PasswordAgain String
 
-passwordsMatch model = model.password == model.passwordAgain
-
-update : Msg -> Model -> Model
 update msg model =
   case msg of
+    Reset ->
+      newModel
+
     Submit ->
-      { model | viewMode = if passwordsMatch model then GoodPassword else BadPassword }
+      { model | viewMode = if passwordsMatch model then Done else YDumb }
 
     Name name ->
       { model | name = name }
 
     Password password ->
-      { model | password = password }
+      { model | password = password, viewMode = viewMode password model.passwordAgain }
 
     PasswordAgain password ->
-      { model | passwordAgain = password }
-
+      { model | passwordAgain = password, viewMode = viewMode model.password password }
 
 -- VIEW
 
-view : Model -> Html Msg
 view model =
   case model.viewMode of
-    StillEntering -> 
-      enterForm model
-    
+    ZeroPassword ->
+      viewNoValidation model
+
     BadPassword ->
       viewBadValidation model
 
     GoodPassword ->
-      viewGoodValidationMessage model
+      viewGoodValidation model
+
+    Done ->
+      viewDoneMessage model
+
+    YDumb ->
+      viewYDumbMessage model
 
 enterForm model = 
   div []
-    [ input [ type' "text", placeholder "Name", onInput Name ] []
-    , input [ type' "password", placeholder "Password", onInput Password ] []
-    , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , button [ onClick Submit ] [ text "Submit" ]
-    ]
+    [ input [ type' "text", placeholder "Name", content model.name, onInput Name ] [], 
+      input [ type' "password", placeholder "Password", content model.password, onInput Password ] [], 
+      input [ type' "password", placeholder "Re-enter Password", content model.passwordAgain, onInput PasswordAgain ] [], 
+      button [ onClick Submit ] [ text "Submit" ] ]
 
-viewGoodValidationMessage model =
+viewNoValidation model =
+  div []
+    [ (enterForm model) ]
+
+viewGoodValidation model =
+  div []
+    [ (enterForm model), (viewGoodValidationMessage) ]
+
+viewBadValidation model =
+  div []
+    [ (enterForm model), (viewBadValidationMessage) ]
+
+viewGoodValidationMessage =
   let (color, message) =
     ("green", "OK")
   in viewValidationMessage color message
 
-viewBadValidationMessage model = 
+viewBadValidationMessage = 
   let (color, message) =
     ("red", "Passwords do not match!")
   in viewValidationMessage color message
@@ -86,6 +110,16 @@ viewBadValidationMessage model =
 viewValidationMessage color message = 
   div [ style [("color", color)] ] [ text message ]
 
-viewBadValidation model =
-  div []
-    [ (enterForm model), (viewBadValidationMessage model) ]
+viewDoneMessage model =
+  let (color, message) =
+    ("green", "You're all set!")
+  in viewValidationMessage color message
+
+viewYDumbMessage model =
+  let (color, message) = 
+    ("red", "Y' Dumb!  Didn't you see the sign??")
+  in 
+    div [] 
+      [ (viewValidationMessage color message),
+        button [ onClick Reset ] [ text "Try Again" ] ]
+  
